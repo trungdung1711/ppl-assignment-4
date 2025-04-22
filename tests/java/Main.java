0358541fc374984befc347e7d9dcdde6e65b03c7
zun <trungdunglebui17112004@gmail.com>
2025-04-22 10:14:59 +0700

    dung-adding the tests for using classfile-analyzer to test the assembly-like code used (jasmin code)

---
+++ tests/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    public static void foo() {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+    }
+}
